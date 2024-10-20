41f8510d6c267df459d00ca67127a06762d07dee
watsontaka <139229993+watsontaka@users.noreply.github.com>
2024-10-20 19:23:24 +0900

    setting2

--- machinelearning_app/picture_classification/views.py
+++ machinelearning_app/picture_classification/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 def index_view(request):
 
-  return render(request, 'index.html',)
+  my_dicts = {
+    'title':'画像認識プログラム',
+    'name':'磯野貴慎',
+  }
 
-# Create your views here.
+  return render(request, 'index.html', my_dicts)
